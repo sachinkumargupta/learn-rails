45746d804158d21b6cb3e629abdb21f9ec404980
sachinkumargupta <sachin7870020331@gmail.com>
2017-09-01 02:35:36 +0530

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,7 @@
-class VisitorsController < ActionController::Base
+class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
 end
